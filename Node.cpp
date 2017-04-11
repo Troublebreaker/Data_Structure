56e0ea1eb1ccb619f529226750007cecffed1c0b
Steve <www.1364270947@qq.com>
2017-04-12 00:04:54 +0800

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Node
+{
+public:
+	Node(char data=0);
+	char m_cData;
+	bool m_bIsVisited;
+
+};
+
+#endif
+
+
+#include"Node.h"
+
+Node::Node(char data)
+{
+	m_cData = data;
+	m_bIsVisited = false;
+}
